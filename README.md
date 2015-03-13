4d5422233563889797d3ba30138ccbff77a6d6a2
Eric W. Wallace <e@ewall.org>
2015-03-12 20:31:30 -0400

    Generic Client fetching one Patient

---
+++ README.md
@@ -0,0 +1,4 @@
+fhir-starter
+============
+
+Learning some client basics of the [HAPI-FHIR API](https://jamesagnew.github.io/hapi-fhir/) from the [University Health Network](http://www.uhn.ca/).
--- src/main/java/org/ewall/app/App.java
+++ src/main/java/org/ewall/app/App.java
@@ -1,13 +1,16 @@
 package org.ewall.app;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.api.Bundle;
+import ca.uhn.fhir.model.dstu.composite.AddressDt;
+import ca.uhn.fhir.model.dstu.composite.HumanNameDt;
 import ca.uhn.fhir.model.dstu.resource.Patient;
 import ca.uhn.fhir.model.primitive.CodeDt;
 import ca.uhn.fhir.model.primitive.StringDt;
-import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.client.IGenericClient;
 
 /**
- * WTF, world?
+ * What the duck, world?
  */
 public class App 
 {
@@ -15,36 +18,43 @@ public class App
     {
         System.out.println( "Let's get this FHIR started!" );
 
-        // from https://jamesagnew.github.io/hapi-fhir/doc_intro.html "Parsing a resource from a String"
+        // warning: slf4j-simple will log messages to stdout here
         
         FhirContext ctx = new FhirContext();
 
-        // The following is an example Patient resource
-		String msgString = "<Patient xmlns=\"http://hl7.org/fhir\">"
-		  + "<text><status value=\"generated\" /><div xmlns=\"http://www.w3.org/1999/xhtml\">John Cardinal</div></text>"
-		  + "<identifier><system value=\"http://orionhealth.com/mrn\" /><value value=\"PRP1660\" /></identifier>"
-		  + "<name><use value=\"official\" /><family value=\"Cardinal\" /><given value=\"John\" /></name>"
-		  + "<gender><coding><system value=\"http://hl7.org/fhir/v3/AdministrativeGender\" /><code value=\"M\" /></coding></gender>"
-		  + "<address><use value=\"home\" /><line value=\"2222 Home Street\" /></address><active value=\"true\" />"
-		  + "</Patient>";
-		 
-		// The hapi context object is used to create a new XML parser
-		// instance. The parser can then be used to parse (or unmarshall) the
-		// string message into a Patient object
-		IParser parser = ctx.newXmlParser();
-		Patient patient = parser.parseResource(Patient.class, msgString);
-		 
-		// The patient object has accessor methods to retrieve all of the
-		// data which has been parsed into the instance. All of the
-		// FHIR datatypes are represented by classes which end in "Dt".
-		StringDt patientId = patient.getIdentifier().get(0).getValue();
-		StringDt familyName = patient.getName().get(0).getFamily().get(0);
-		CodeDt gender = patient.getGender().getCoding().get(0).getCode();
-		 
-		// The various datatype classes have accessors called getValue()
-		System.out.println(patientId.getValue()); // PRP1660
-		System.out.println(familyName.getValue()); // Cardinal
-		System.out.println(gender.getValue()); // M
+        String serverBase = "http://fhirtest.uhn.ca/base";
+        IGenericClient client = ctx.newRestfulGenericClient(serverBase);
+         
+        // Perform a search
+        Bundle results = client
+              .search()
+              .forResource(Patient.class)
+              .where(Patient.FAMILY.matches().value("duck"))
+              .execute();
+         
+        System.out.println("Found " + results.size() + " patients named 'duck'. Here's the first one:\n");
+        
+        Patient patient = results.getResources(Patient.class).get(0);
 
+	    StringDt patientId = patient.getIdentifier().get(0).getValue();
+	    System.out.println("Patient Id:  " + patientId.getValue()); // 7000135
+
+	    HumanNameDt name = patient.getName().get(0);
+	    System.out.println("First Name:  " + name.getGiven().get(0).getValue()); // Donald
+	    System.out.println("Last Name:   " + name.getFamily().get(0).getValue()); // Duck
+
+	    System.out.println("Birth Date:  " + patient.getBirthDate().getValueAsString()); // 1980-06-01
+	    
+	    CodeDt gender = patient.getGender().getCoding().get(0).getCode();
+	    System.out.println("Gender:      " + gender.getValue()); // M
+	    
+	    AddressDt address = patient.getAddress().get(0);
+	    System.out.println("Address:     " + address.getLine().get(0));
+	    System.out.println("             " + address.getCity().getValue() + ", "
+	    								   + address.getState().getValue() + " "
+	    								   + address.getZip().getValue());
+	    System.out.println("             " + address.getCountry().getValue());
+	    
+	    //System.out.println("Married?:    " + patient.getMaritalStatus().getCoding().get(0).getCode().getValue());
     }
 }
