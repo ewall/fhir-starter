53d46d091b63b508062fc4b04081598976d28fd4
Eric W. Wallace <e@ewall.org>
2015-03-17 23:27:09 -0400

    added Observations; need to prove out Conditions and MedicationPrescriptions

--- src/main/java/org/ewall/app/App.java
+++ src/main/java/org/ewall/app/App.java
@@ -1,13 +1,21 @@
 package org.ewall.app;
 
+import java.util.List;
+
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.IGenericClient;
 import ca.uhn.fhir.model.api.Bundle;
+import ca.uhn.fhir.model.dstu.resource.Patient;
+import ca.uhn.fhir.model.dstu.resource.Observation;
+import ca.uhn.fhir.model.dstu.resource.Condition;
+import ca.uhn.fhir.model.dstu.resource.MedicationPrescription;
+import ca.uhn.fhir.model.primitive.CodeDt;
+import ca.uhn.fhir.model.primitive.DateTimeDt;
+import ca.uhn.fhir.model.primitive.IdDt;
+import ca.uhn.fhir.model.primitive.StringDt;
 import ca.uhn.fhir.model.dstu.composite.AddressDt;
 import ca.uhn.fhir.model.dstu.composite.HumanNameDt;
-import ca.uhn.fhir.model.dstu.resource.Patient;
-import ca.uhn.fhir.model.primitive.CodeDt;
-import ca.uhn.fhir.model.primitive.StringDt;
-import ca.uhn.fhir.rest.client.IGenericClient;
+import ca.uhn.fhir.model.dstu.composite.QuantityDt;
 
 /**
  * What the duck, world?
@@ -18,43 +26,116 @@ public class App
     {
         System.out.println( "Let's get this FHIR started!" );
 
-        // warning: slf4j-simple will log messages to stdout here
+        // warning: slf4j-simple will log messages to stdout
         
         FhirContext ctx = new FhirContext();
 
-        String serverBase = "http://fhirtest.uhn.ca/base";
+        String serverBase = "http://fhirtest.uhn.ca/baseDstu1";
         IGenericClient client = ctx.newRestfulGenericClient(serverBase);
          
         // Perform a search
-        Bundle results = client
+        String query="duck";
+        Bundle patients = client
               .search()
               .forResource(Patient.class)
-              .where(Patient.FAMILY.matches().value("duck"))
+              .where(Patient.FAMILY.matches().value(query))
               .execute();
-         
-        System.out.println("Found " + results.size() + " patients named 'duck'. Here's the first one:\n");
+        System.out.println("Found " + patients.size() + " patients named '" + query + "'.\n");
+        //Patient patient = results.getResources(Patient.class).get(0);
         
-        Patient patient = results.getResources(Patient.class).get(0);
+        // Get specific Patient ID
+        String resid = "d1132446701";
+        System.out.println("Fetching Patient with RES_ID '" + resid + "'.");
+        IdDt id = new IdDt(resid);
+        Patient patient = client.read(Patient.class, id);
 
 	    StringDt patientId = patient.getIdentifier().get(0).getValue();
-	    System.out.println("Patient Id:  " + patientId.getValue()); // 7000135
+	    System.out.println("Patient Id:  " + patientId.getValue());
 
 	    HumanNameDt name = patient.getName().get(0);
-	    System.out.println("First Name:  " + name.getGiven().get(0).getValue()); // Donald
-	    System.out.println("Last Name:   " + name.getFamily().get(0).getValue()); // Duck
+	    System.out.println("First Name:  " + name.getGiven().get(0).getValue());
+	    System.out.println("Last Name:   " + name.getFamily().get(0).getValue());
 
-	    System.out.println("Birth Date:  " + patient.getBirthDate().getValueAsString()); // 1980-06-01
+	    System.out.println("Birth Date:  " + patient.getBirthDate().getValueAsString());
 	    
 	    CodeDt gender = patient.getGender().getCoding().get(0).getCode();
-	    System.out.println("Gender:      " + gender.getValue()); // M
+	    System.out.println("Gender:      " + gender.getValue());
+		
+	    System.out.println("Married?:    " + patient.getMaritalStatus().getCodingFirstRep().getCode().getValue());
 	    
-	    AddressDt address = patient.getAddress().get(0);
-	    System.out.println("Address:     " + address.getLine().get(0));
-	    System.out.println("             " + address.getCity().getValue() + ", "
-	    								   + address.getState().getValue() + " "
-	    								   + address.getZip().getValue());
-	    System.out.println("             " + address.getCountry().getValue());
+   	    List<AddressDt> address = patient.getAddress();
+	    if (address!=null & !address.isEmpty()) {
+		    System.out.println("Address:     " + address.get(0).getLine().get(0));
+		    System.out.println("             " + address.get(0).getCity().getValue() + ", "
+		    								   + address.get(0).getState().getValue() + " "
+		    								   + address.get(0).getZip().getValue());
+		    System.out.println("             " + address.get(0).getCountry().getValue());
+		}
 	    
-	    //System.out.println("Married?:    " + patient.getMaritalStatus().getCoding().get(0).getCode().getValue());
+	    // Get Observations
+	    Bundle observations = client
+	    		.search()
+	    		.forResource(Observation.class)
+	    		.where(Observation.SUBJECT.hasId(id))
+	    		.execute();
+	    if (observations.size() > 0) {
+	    	System.out.println("\nFound " + observations.size() + " observations for this patient. Here's the first one:\n");
+	    	
+	    	Observation obs = observations.getResources(Observation.class).get(0);
+	    	
+	    	System.out.println("Name:        " + obs.getNameElement().getCodingFirstRep().getDisplayElement().getValue());
+	    	System.out.println(" - System:   " + obs.getNameElement().getCodingFirstRep().getSystemElement().getValue());
+	    	System.out.println(" - Code:     " + obs.getNameElement().getCodingFirstRep().getCodeElement().getValue());
+
+	    	QuantityDt quan = (QuantityDt) obs.getValue();
+	    	System.out.println("Value:       " + quan.getValueElement().getValueAsString());
+	    	System.out.println("Units:       " + quan.getUnitsElement().getValueAsString());
+	    	
+	    	DateTimeDt applies = (DateTimeDt) obs.getAppliesElement();
+	    	System.out.println("Applies:     " + applies.getValueAsString());
+	    	
+	    	System.out.println("Performer:   " + obs.getPerformerElement().get(0).getDisplay().getValue());
+	    } else {
+	    	System.out.println("\nSorry, no observations found for this patient.");
+	    }
+	    
+	    // Get Conditions
+	    Bundle conditions = client
+	    		.search()
+	    		.forResource(Condition.class)
+	    		.where(Condition.SUBJECT.hasId(id))
+	    		.execute();
+	    if (conditions.size() > 0) {
+	    	System.out.println("\nFound " + conditions.size() + " conditions for this patient. Here's the first one:\n");
+	    	
+	    	Condition cond = conditions.getResources(Condition.class).get(0);
+	    	
+	    	System.out.println("Subject:     " + cond.getSubject().getDisplay().getValue());
+	    	
+	    	// TODO: explore conditions data
+	    	
+	    } else {
+	    	System.out.println("\nSorry, no conditions found for this patient.");
+	    }
+	    
+	    // Get MedicationPrescription
+	    Bundle prescriptions = client
+	    		.search()
+	    		.forResource(MedicationPrescription.class)
+	    		.where(MedicationPrescription.PATIENT.hasId(id))
+	    		.execute();
+	    if (prescriptions.size() > 0) {
+	    	System.out.println("\nFound " + prescriptions.size() + " prescriptions for this patient. Here's the first one:\n");
+	    	
+	    	MedicationPrescription rx = prescriptions.getResources(MedicationPrescription.class).get(0);
+	    	
+	    	System.out.println("Display:     " + rx.getMedicationElement().getDisplay().getValue());
+	    	
+	    	// TODO: explore prescriptions data
+	    	
+	    } else {
+	    	System.out.println("\nSorry, no prescriptions found for this patient.");
+	    }
+	    
     }
 }
